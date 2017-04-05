3776031cb9e26e3e09e3dcdee390f3787229a11d
iqbalansyori <iqbal@icehousecorp.com>
2017-04-05 20:54:20 +0700

    Add carthage support - share scheme

---
+++ Example/Pods/Target Support Files/Pods-DTLoadingButton_Example/Pods-DTLoadingButton_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_DTLoadingButton_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DTLoadingButton_ExampleVersionString[];
+
